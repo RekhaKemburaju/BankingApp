a9592837ad00ef9dfdbadf6ccc0a0ca980fe5883
rekha <rekha@LAPTOP-KICOOLK2>
2022-09-06 12:26:27 +1000

    Add project files.

---
+++ Bank/BusinessCustomer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Bank
+{
+    public class BusinessCustomer :Customer
+    {
+        private String ABN;
+
+        public BusinessCustomer(string name,string address ,string ABN):base(name,address)
+        {
+            this.ABN = ABN;
+        }
+         public String _ABN
+        {
+            get { return ABN; }
+            set { ABN = value; }
+        }
+        public void showBusinessCustomerDetails()
+        {
+            Console.WriteLine("Customer Name: " + base.CustName);
+            Console.WriteLine("Customer Address: " + base.CustAddress);
+            Console.WriteLine("ABN: " + ABN);
+
+        }
+
+
+    }
+}
---
+++ Bank/PersonalCustomer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Bank
+{
+    public class PersonalCustomer:Customer
+    {
+        private string TFN;
+        private DateTime DOB;
+
+        public PersonalCustomer(string name,string address, string tfn, DateTime dOB):base (name, address)
+        {
+            this.TFN = tfn;
+            DOB = dOB;
+          
+        }
+        public string _TFN
+        {
+            get => TFN;
+            set => TFN = value;
+        }
+        public DateTime getDOB()
+        {
+            return DOB;
+        }
+        public void setDOB(String dateString)
+        {
+            DOB = DateTime.Now;
+        }
+        public void showBusinessCustomerDetails()
+        {
+            Console.WriteLine("Customer Name: " + base.CustName);
+            Console.WriteLine("Customer Address: " + base.CustAddress);
+            Console.WriteLine("ABN: " + TFN);
+            Console.WriteLine("DOB :" + DOB);
+
+        }
+
+    }
+}
